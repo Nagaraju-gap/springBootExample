fa1ac8a59840ad957209a3d36685b25c7b01f0fe
na89991 <nagaraju_javvaji@gap.com>
2016-02-03 16:52:28 -0800

    renamed the main class

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -12,10 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
-public class HelloWorld{
+public class Application{
 
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(HelloWorld.class, args);
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
     }
 
     @RequestMapping("/")
